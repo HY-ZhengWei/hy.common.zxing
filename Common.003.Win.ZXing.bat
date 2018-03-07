45eb92d4ff2fc87849ca43b712b91843fdf9dd60
HY-ZhengWei <hy.zhengwei@qq.com>
2018-03-07 12:12:10 +0800

    3.3.1

--- Common.003.Win.ZXing.bat
+++ Common.003.Win.ZXing.bat
@@ -2,6 +2,7 @@
 
 cd .\bin
 
+
 rd /s/q .\org\hy\common\zxing\junit
 
 
@@ -11,3 +12,12 @@ copy hy.common.zxing.jar ..
 del /q hy.common.zxing.jar
 cd ..
 
+
+
+
+
+cd .\src
+jar cvfm hy.common.zxing-sources.jar MANIFEST.MF META-INF com org 
+copy hy.common.zxing-sources.jar ..
+del /q hy.common.zxing-sources.jar
+cd ..
--- Common.004.Mac.ZXing.sh
+++ Common.004.Mac.ZXing.sh
@@ -2,6 +2,7 @@
 
 cd ./bin
 
+
 rm -R ./org/hy/common/zxing/junit
 
 
@@ -11,3 +12,12 @@ cp hy.common.zxing.jar ..
 rm hy.common.zxing.jar
 cd ..
 
+
+
+
+
+cd ./src
+jar cvfm hy.common.zxing-sources.jar MANIFEST.MF META-INF com org 
+cp hy.common.zxing-sources.jar ..
+rm hy.common.zxing-sources.jar
+cd ..
